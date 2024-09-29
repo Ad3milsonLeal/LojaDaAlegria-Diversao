ba108f35f9653b4764f8005e286dbae17bd201e4
Ad3milsonLeal <163893460+Ad3milsonLeal@users.noreply.github.com>
2024-09-29 20:20:48 -0300

    LojaDaAlegria&Diversão

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para redirecionar para a tela secundária
+function confirmar() {
+    window.location.href = "telasecund.html"; // Substitua pelo nome do seu arquivo HTML secundário
+}
+
+// Função para redirecionar de volta para a tela inicial
+function voltarMenuInicial() {
+    window.location.href = "index.html"; // Substitua pelo nome do seu arquivo HTML inicial
+}
